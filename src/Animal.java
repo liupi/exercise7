b666ce171ff7e37e3a50cbc5fe5b4feff3b70940
Liudmila <milalindley@gmail.com>
2025-11-25 18:06:54 +0100

    feat: create class Animal

---
+++ src/Animal.java
@@ -0,0 +1,45 @@
+abstract class Animal {
+    private String name;
+    private int age;
+    private int weight;
+
+    public Animal() {
+        this.name = "";
+    }
+
+    public Animal(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public int getWeight() {
+        return weight;
+    }
+
+    public abstract void eat();
+    public abstract void getVoice();
+}
